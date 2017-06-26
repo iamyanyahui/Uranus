858c1d6f8ad319429c2c4403f8bcb06a36d8f6c2
yangjiawei <yangjiawei@xiaomi.com>
2017-06-26 14:37:36 +0800

    small changes
    Signed-off-by: yangjiawei <yangjiawei@xiaomi.com>

---
+++ app/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from app.models import *
+
+admin.site.register(User)
+admin.site.register(Term)
+admin.site.register(TeamMeta)
+admin.site.register(Course)
+admin.site.register(Enroll)
+admin.site.register(Team)
+admin.site.register(Member)
+admin.site.register(WorkMeta)
+admin.site.register(Work)
+admin.site.register(File)
+admin.site.register(Attachment)
+admin.site.register(Log)
--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # [用户:学生/教师/教务账户]
 class User(models.Model):
     username = models.CharField(unique=True, max_length=32, help_text='学号/工号')
@@ -20,6 +21,7 @@ class User(models.Model):
     tel = models.CharField(max_length=16, help_text='电话')
     email = models.CharField(max_length=64)
 
+
 # [学期]
 class Term(models.Model):
     info = models.TextField(help_text='学期说明信息')
@@ -33,6 +35,7 @@ class Term(models.Model):
     endTime = models.DateTimeField()
     # TODO: 周次安排？！！
 
+
 # [团队元信息]
 class TeamMeta(models.Model):
     minNum = models.PositiveSmallIntegerField(default=1)
@@ -40,10 +43,11 @@ class TeamMeta(models.Model):
     startTime = models.DateTimeField(help_text='允许组队的开始时间')
     endTime = models.DateTimeField()
 
+
 # [课程]==[学期]&[团队元信息]
 class Course(models.Model):
-    term = models.ForeignKey(Term)         # 外键：学期
-    teamMeta = models.ForeignKey(TeamMeta)    # 外键：团队元信息
+    term = models.ForeignKey(Term)  # 外键：学期
+    teamMeta = models.ForeignKey(TeamMeta)  # 外键：团队元信息
     name = models.CharField(max_length=64)
     info = models.TextField(help_text='课程要求/其他说明')
     syllabus = models.TextField(help_text='课程大纲')
@@ -58,10 +62,12 @@ class Course(models.Model):
     startTime = models.DateTimeField()
     endTime = models.DateTimeField()
 
+
 # <选课>==[课程]&[用户:学生/教师账户]
 class Enroll(models.Model):
     course = models.ForeignKey(Course)
     user = models.ForeignKey(User)
+
 
 # [团队]==[课程]&[用户:学生账户]
 class Team(models.Model):
@@ -76,6 +82,7 @@ class Team(models.Model):
     status = models.CharField(max_length=16, choices=STATUS, default='unsubmitted')
     info = models.TextField(help_text='通过欢迎信息/驳回理由')
 
+
 # <团队成员>==[团队]&[用户:学生账户]
 class Member(models.Model):
     team = models.ForeignKey(Team)
@@ -87,6 +94,7 @@ class Member(models.Model):
     role = models.CharField(max_length=16, choices=ROLE, default='member')
     contribution = models.FloatField(help_text='成员贡献度:0.4~1.2')
 
+
 # [作业任务]~~<附件>
 class WorkMeta(models.Model):
     user = models.ForeignKey(User, help_text='发布者:教师')
@@ -96,6 +104,7 @@ class WorkMeta(models.Model):
     startTime = models.DateTimeField()
     endTime = models.DateTimeField()
 
+
 # [作业提交]~~<附件>
 class Work(models.Model):
     workMeta = models.ForeignKey(WorkMeta, help_text='作业任务元信息')
@@ -103,6 +112,7 @@ class Work(models.Model):
     content = models.TextField()
     review = models.TextField(help_text='教师简评')
     score = models.FloatField(help_text='得分: 0.0~10.0')
+
 
 # [资源文件]
 class File(models.Model):
@@ -116,6 +126,7 @@ class File(models.Model):
     )
     type = models.CharField(max_length=16, choices=TYPE, default='text')
 
+
 # <附件>==[作业任务|作业提交]&[资源文件]
 class Attachment(models.Model):
     file = models.ForeignKey(File)
@@ -127,11 +138,13 @@ class Attachment(models.Model):
     )
     type = models.CharField(max_length=16, choices=TYPE, default='workmeta')
 
+
 # [日志]
 class Log(models.Model):
     user = models.ForeignKey(User, help_text='操作员')
     event = models.TextField(help_text='事件记录')
     time = models.DateTimeField(auto_now_add=True)
+
 
 # [签到]
 class Attendance(models.Model):

b7c08a3af31f59257b439ccaf2998f7830df5942
yangjiawei <yangjiawei@xiaomi.com>
2017-06-26 10:36:56 +0800

    delete useless dirs
    Signed-off-by: yangjiawei <yangjiawei@xiaomi.com>

--- student/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Student(models.Model):
-    username = models.CharField(null=False, unique=True, max_length=15, help_text='用户名，用于登陆')
-    # 这玩意儿就是学号吧
-    password = models.CharField(null=False, max_length=32, )
